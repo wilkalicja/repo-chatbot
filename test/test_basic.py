48d4f6e57e4c6d7b9c96c588353c6d6a2463e7b1
mdovgialo <marian.dovgialo@fuw.edu.pl>
2022-03-02 14:38:49 +0100

    Fixed tests

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,7 +1,10 @@
 """Basic tests to test testing system."""
-import uaal
+import example_project
 
 
 def test_import():
     """Package testing stub."""
-    assert uaal.TEST_VAR == 1
+    assert example_project.TEST_VAR == 1
+    
+def test_function():
+    assert example_project.f1() == 1
