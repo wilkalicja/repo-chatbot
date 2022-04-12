76b403fd3f174feec930741e273fbbeaff5c01fc
Mateusz  Paszkiewicz <mpaszkiewicz2001@gmail.com>
2022-04-12 09:29:49 +0000

    Dev#10 wczytywanie zapytania

--- fuw_chatbot/test.py
+++ fuw_chatbot/test.py
@@ -1,4 +1,27 @@
+import nltk
 import morfeusz2
+
+nltk.download('punkt')  ## potrzebne zeby odpalic world_tokenize
+
+
+def powitanie():
+    print('Witaj! Nazywam sie FUWChatbot i służę swoją pomocą! W czym mogę Ci pomóc?')
+
+
+def wczytywanie_zapytania():
+    odpowiedz_uzytkownika = input()
+    zapytanie = nltk.word_tokenize(
+        odpowiedz_uzytkownika)  # okazuje sie byc lepsza funklcja niz split bo oddziela fajnie interpunkcje od slow
+    # usuwanie interpukncji:
+    licz = len(zapytanie)
+    zapytanie = filter(lambda c: c not in {',','.','?', '!', ':', ';'}, zapytanie)  #funkcja filter filtruje liste z tych znakow
+    print(list(zapytanie))  #ona tworzy obiekt i zeby dzialalo trzeba dac list() zrodlo: https://www.simplilearn.com/tutorials/python-tutorial/filter-in-python
+
+
+powitanie()
+wczytywanie_zapytania()
+
+
 
 def analiza_morfologiczna(s):   #niestety dziala tylko z poprawnie zlozonymi slowami ; zaprojektowalem to tak zeby bralo tylko pierwsze slowo ze stringa poniewaz wczytywanie zapisuje mi liste slow i chce przekazywac slowa po kolei
     morf = morfeusz2.Morfeusz()
