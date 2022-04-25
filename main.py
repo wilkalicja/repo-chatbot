b45bf5fbf7a4175d4a510a84c71a207c30d13cc6
alicjawilk <alicjawilk.poczta@gmail.com>
2022-04-25 14:41:45 +0200

    .

---
+++ main.py
@@ -0,0 +1,52 @@
+{"tagi":[
+    {"tag": "powitanie",
+    "pytania": ["Hej","Cześć","Co tam?"],
+    "odpowiedzi":"Witaj"
+    },
+    {
+    "tag": "biblioteka godziny otwarcia",
+    "pytania": ["W jakich godzinach jest otwarta biblioteka","kiedy jest otwarta biblioteka","kiedy mogę iść do biblioteki"],
+    "odpowiedzi":"Biblioteka jest czynna w godzinach 10-18 od pon do pt"
+    },
+    {
+    "tag": "biblioteka sprawdzenie dostępności",
+    "pytania": ["czy moja książka jest dostępna","gdzie mogę wypożyczyć tą książkę"],
+    "odpowiedzi":"Aby sprawdzić dostępność wybranej książki wejdź na stronę BUW i wpisz autora lub tutuł "
+    },
+    {
+    "tag": "biblioteka przedłużenie książki",
+    "pytania": ["jak przedłużyć ksążkę", "gdzie mogę przedłużyć książkę", "moja książka "],
+    "odpowiedzi": "Aby dokonać prologanty wybranej książki wejdź na stronę BUW, zaloguj się na swoje konto i wybierz pozycje, które chcesz przedłużyć. Możesz też udać się do biblioteki lub zadzonić pod nr ... "
+    },
+    {
+    "tag": "drukowanie",
+    "pytania": ["czy mogę coś wydrukować na FUW", "gdzie mogę wydrukować dokument"],
+    "odpowiedzi": "Na FUW-ie masz możliwość wydruku w bibliotece lub w Makerspace. Pamiętaj o zabraniu własnego pendrive z plikiem, zaoszczędzisz czas"
+    },
+    {"tag": "druk 3D",
+     "pytania": ["czy mogę wydrukować coś samodzielnie na drukarce 3D", "dla kogo są dostępne drukarki 3D", "czy można rezerwować drukarki 3D"],
+     "odpowiedzi": "Drukarki 3D są dostępne dla każdego, jednak najpierw upewnij się, że masz potrzebne uprawnienia. Możesz to sprawdzić na stronie ."
+     },
+    {"tag": "druk 3D uprawnienia",
+     "pytania": ["nie mam uprawnień na drukarkę 3D", "czemu nie mogę zarezerwować drukarki"],
+     "odpowiedzi": "Jeśli nie możesz zarezerwować drukarki 3D to prawdopodobnie nie masz potrzebnych uprawnień lub Twoje konto jest nieaktywne. "
+    "Aby uzyskać uprawnienia na druk 3D trzeba odbyć obowiązkowe 4 - godzinne szkolenie na którym poznasz podstawy działania drukarek oraz zasady bezpiecznej pracy "
+     },
+    {"tag": "nieaktywne konto rezerwacje",
+     "pytania": ["mam nieaktywne konto"],
+     "odpowiedzi": "Aby móc dokonywać rezerwacji Twoje konto musi być aktywne, uzupełnij inforamcje o sobie, wyślij i poczekaj na zatwierdzenie. Po poprawnej weryfikacji będziesz mógł korzytsać z rezerwacji"
+     },
+    {"tag": "salki PWN",
+     "pytania": ["dla kogo są salki PWN", "czy mogę uczyć się w salce", "czy mogę zarezerwować salke"],
+     "odpowiedzi": "Salki PWN zostały stworzone specjalnie dla Was i mogą być rezerwowane przez studentów. Możesz zarezerwować salkę dla siebie lub uczyć się z kolegami i  koleżnakami. Jeśli nie wiesz gdzie zarezerwować salkę wpisz - rezerwacja salek PWN - "
+     },
+    {"tag": "rezerwacja salek PWN",
+     "pytania": ["jak zarezerwować PWN", "", "Co tam?"],
+     "odpowiedzi": "Witaj"
+     },
+     {"tag": "dziekan pokój",
+     "pytania": ["Gdzie Dziekan przyjmuje studentów", "nr sali gdzie przyjmuje Dziekan"],
+     "odpowiedzi": "Nasz Dziekan ds. Studenckich przyjmuje w pokoju 1.20 w budynku A"
+     },
+
+]}
